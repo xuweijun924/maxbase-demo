d48da401efeb85c62259d8e10f0d5612500830f8
徐胜祥 <shengxiang.xu@maxnerva.com>
2022-03-21 15:37:31 +0800

    调整 InitializingBean 的使用规范

--- src/main/java/com/maxnerva/maxbase/demo/common/component/MaxbaseDemoInitializingBean.java
+++ src/main/java/com/maxnerva/maxbase/demo/common/component/MaxbaseDemoInitializingBean.java
@@ -1,12 +1,13 @@
 package com.maxnerva.maxbase.demo.common.component;
 
-import com.maxnerva.maxbase.demo.common.exception.initializing.ExceptionParameterInitializingBean;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.stereotype.Component;
 
 /**
- * 应用初始化逻辑
+ * 应用初始化操作，在此实现一般有 2 种情况：<br>
+ * 1. 属于应用全局的初始化处理（如：加载 DB 数据到内存）；<br>
+ * 2. 多个初始化操作分散处理并执行时，无法保证初始化顺序，而集中在此处理则可解决。
  *
  * @author Shengxiang Xu
  * @date 3/17/2022
@@ -15,11 +16,8 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class MaxbaseDemoInitializingBean implements InitializingBean {
 
-    private final ExceptionParameterInitializingBean exceptionParameterInitializingBean;
-
     @Override
     public void afterPropertiesSet() throws Exception {
-        exceptionParameterInitializingBean.setFromParameter();
         // ...
     }
 
--- src/main/java/com/maxnerva/maxbase/demo/common/exception/initializing/ExceptionParameterInitializingBean.java
+++ src/main/java/com/maxnerva/maxbase/demo/common/exception/initializing/ExceptionParameterInitializingBean.java
@@ -2,6 +2,7 @@ package com.maxnerva.maxbase.demo.common.exception.initializing;
 
 import cn.hutool.core.util.StrUtil;
 import com.maxnerva.maxbase.common.exception.base.SystemParameterProvider;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -10,15 +11,17 @@ import org.springframework.stereotype.Component;
  * @date 3/17/2022
  */
 @Component
-public class ExceptionParameterInitializingBean {
+public class ExceptionParameterInitializingBean implements InitializingBean {
 
     @Value("${spring.application.name}")
     private String serviceName;
 
-    public void setFromParameter() {
+    @Override
+    public void afterPropertiesSet() {
         if (StrUtil.isBlank(serviceName)) {
             throw new IllegalArgumentException("${spring.application.name} must not be blank");
         }
+        // 用于配合 Maxbase-min 脚手架显示异常信息（from，用于显示异常抛出的位置）
         SystemParameterProvider.getInstance().setServiceName(serviceName);
     }
 
